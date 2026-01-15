d024a634f8542ae4cda89bf56bd79708b0769d25
Your Name <you@example.com>
2026-01-15 15:01:51 -0600

    Update server with full proxy and catch-all routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Player from './pages/Player';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* 1. When the URL is just "/", show the Library Grid */}
-        <Route path="/" element={<Home />} />
-        
-        {/* 2. When the URL is "/book/123", show the Player Page */}
-        <Route path="/book/:id" element={<Player />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="bg-[#0f172a] min-h-screen">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* This line is what is currently missing and causing the error */}
+          <Route path="/player/:id" element={<Player />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
